a14f92ec2a29eba7153d1049723e0e6469c003e2
Nishit <nishitjoshi.ichs@gmail.com>
2023-03-02 02:17:23 +0000

    added IDs for each hours to make them persist after reload

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,19 @@ $(document).ready(function () {
         console.log(x);
         localStorage.setItem(y, x) 
         localStorage.getItem()
+        localStorage.setItem('y', x) 
+        var z = localStorage.getItem('y')
     })
-    $("#txt").val(localStorage.getItem("hour-13"))
+
+    $("#txt1").val(localStorage.getItem("hour-9"))
+    $("#txt2").val(localStorage.getItem("hour-10"))
+    $("#txt3").val(localStorage.getItem("hour-11"))
+    $("#txt4").val(localStorage.getItem("hour-12"))
+    $("#txt5").val(localStorage.getItem("hour-13"))
+    $("#txt6").val(localStorage.getItem("hour-14"))
+    $("#txt7").val(localStorage.getItem("hour-15"))
+    $("#txt8").val(localStorage.getItem("hour-16"))
+    $("#txt9").val(localStorage.getItem("hour-17"))
 
     // FInd out from googling how to grab current time from momentjs
 
